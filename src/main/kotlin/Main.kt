3486a988491df404fac0a5b4949b01a4a168ad3d
guarnerman <gs.ulises18@gmail.com>
2024-05-02 19:59:31 -0600

    Primer commit: hola mundo en kotlin, uso de var, val y const val

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+package org.example
+
+const val PI = 3.141592
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var dinero = 10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+    val nombre = "Diego"
+    println(nombre)
+    println(PI)
+}
